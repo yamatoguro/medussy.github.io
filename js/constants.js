7bb9aa187fc84f132ea33018ce7270a4c8bfc9f6
crazy4blades <151548637+crazy4blades@users.noreply.github.com>
2025-10-22 19:29:46 -0400

    Update race participants and schedule
    Replaced 'Eldri7ch' with 'CRAZY4BLADE' and updated the channel name. Removed the second match and changed the scheduled time for the remaining match to 2025-10-26T18:00:00-04:00.

--- js/constants.js
+++ js/constants.js
@@ -1,16 +1,9 @@
 window.races = [
   {
     match: [
-      { display: "Eldri7ch", channel: "eldri7ch" },
+      { display: "CRAZY4BLADE", channel: "crazy4bladestv" },
       { display: "Renantrl", channel: "renantrl1" }
     ],
-    estTime: "2025-10-21T18:00:00-04:00"
-  },
-  {
-    match: [
-      { display: "ClassicAdvent", channel: "classicadvent" },
-      { display: "Skr1mps", channel: "skr1mps" }
-    ],
-    estTime: "2025-10-22T18:00:00-04:00"
+    estTime: "2025-10-26T18:00:00-04:00"
   }
 ];
