a35eb1c12f9b461bbf1e552743b8fd3ee256e448
crazy4blades <151548637+crazy4blades@users.noreply.github.com>
2025-10-23 09:02:53 -0400

    Add new races to window.races array
    Added two new race entries to the window.races array in js/constants.js, including match details and estimated times for Skr1mps vs Majorseamen and Alex P vs Eldri7ch.

--- js/constants.js
+++ js/constants.js
@@ -1,8 +1,20 @@
 window.races = [
   {
     match: [
+      { display: "Skr1mps", channel: "skr1mps" },
+      { display: "Majorseamen", channel: "majorsemen" }
+    ],
+    estTime: "2025-10-24T18:00:00-04:00"
+  },{
+    match: [
       { display: "CRAZY4BLADES", channel: "crazy4bladestv" },
       { display: "Renantrl", channel: "renantrl1" }
+    ],
+    estTime: "2025-10-26T18:00:00-04:00"
+  },{
+    match: [
+      { display: "Alex P", channel: "alexpi_fgc" },
+      { display: "Eldri7ch", channel: "eldri7ch" }
     ],
     estTime: "2025-10-26T18:00:00-04:00"
   }
