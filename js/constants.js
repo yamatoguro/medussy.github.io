b775edfd695b99ee5a7d54cb42f9c40e7bd3e27d
crazy4blades <151548637+crazy4blades@users.noreply.github.com>
2025-10-23 21:24:25 -0400

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -1,12 +1,6 @@
 window.races = [
   {
     match: [
-      { display: "Skr1mps", channel: "skr1mps" },
-      { display: "Majorseamen", channel: "majorsemen" }
-    ],
-    estTime: "2025-10-24T18:00:00-04:00"
-  },{
-    match: [
       { display: "CRAZY4BLADES", channel: "crazy4bladestv" },
       { display: "Renantrl", channel: "renantrl1" }
     ],
@@ -16,6 +10,12 @@ window.races = [
       { display: "Alex P", channel: "alexpi_fgc" },
       { display: "Eldri7ch", channel: "eldri7ch" }
     ],
-    estTime: "2025-10-26T18:00:00-04:00"
+    estTime: "2025-10-28T18:30:00-04:00"
+  },{
+    match: [
+      { display: "Skr1mps", channel: "skr1mps" },
+      { display: "Majorseamen", channel: "majorsemen" }
+    ],
+    estTime: "2025-10-29T18:00:00-04:00"
   }
 ];
