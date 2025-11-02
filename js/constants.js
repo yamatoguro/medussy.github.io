f5ad9c1a59467a229e22aa717d2d70f939abb2ec
crazy4blades <151548637+crazy4blades@users.noreply.github.com>
2025-11-01 22:07:22 -0400

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -10,6 +10,6 @@ window.races = [
       { display: "Skr1mps", channel: "skr1mps" },
       { display: "CRAZY4BLADES", channel: "crazy4bladestv" }
     ],
-    estTime: "2025-10-29T18:00:00-04:00"
+    estTime: "2025-11-05T18:00:00-05:00"
   }
 ];
