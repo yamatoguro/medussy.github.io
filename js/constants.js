67fbe399e2e6bd8f8a338ba7e064a0b137ea3a4b
crazy4blades <151548637+crazy4blades@users.noreply.github.com>
2025-11-26 20:09:53 -0500

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -3,12 +3,12 @@ window.races = [
   {
     match: [
       { display: "DerDrach", channel: "derdrach" },
-      { display: "Classic Advent", channel: "classicadvent" }
+      { display: "Renantrl", channel: "renantrl1" }
     ],
-    estTime: "2025-11-26T19:00:00-05:00"
+    estTime: "2025-11-29T19:00:00-05:00"
   }//,{
   //   match: [
-  //     { display: "Renantrl", channel: "renantrl1" },
+  //     ,
   //     { display: "CRAZY4BLADES", channel: "crazy4bladestv" }
   //   ],
   //   estTime: "2025-11-16T19:00:00-05:00"
